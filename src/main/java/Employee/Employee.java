f7085bfa28f1fa92cb45093bf5ed03f930a7ca1e
HellHorseman <duke.sheffer@yandex.ru>
2023-05-11 22:34:10 +0400

    Initial commit

---
+++ src/main/java/Employee/Employee.java
@@ -0,0 +1,35 @@
+package Employee;
+public class Employee {
+
+    private String name;
+    private String surname;
+
+    public Employee(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    @Override
+    public String toString() {
+        return "surname='" + surname + '\'' +
+                " name='" + name + '\'' ;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
