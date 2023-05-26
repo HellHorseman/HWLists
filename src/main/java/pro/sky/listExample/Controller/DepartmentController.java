b1c246aac4481fecccac9deb9b90455a00e5aaf6
HellHorseman <duke.sheffer@yandex.ru>
2023-05-26 22:28:21 +0400

    retype methods from Stream & Optional

--- src/main/java/pro/sky/listExample/Controller/DepartmentController.java
+++ src/main/java/pro/sky/listExample/Controller/DepartmentController.java
@@ -1,8 +1,38 @@
 package pro.sky.listExample.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.listExample.Employee.Employee;
+import pro.sky.listExample.Service.DepartmentService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/departments")
 public class DepartmentController {
+    private final DepartmentService departmentService;
 
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+    @GetMapping("/max-salary")
+    public Optional<Employee> maxDeptSal (@RequestParam ("departmentID") Integer department) {
+        return departmentService.maxDeptSal(department);
+    }
+    @GetMapping("/min-salary")
+    public Optional<Employee> minDeptSal (@RequestParam ("departmentID") Integer department) {
+        return departmentService.minDeptSal(department);
+    }
+    @GetMapping(value = "/all",params = {"departmentId"})
+    public List<Employee> printEmployeeToDepartment (@RequestParam ("departmentID") Integer department) {
+        return departmentService.printEmployeeToDepartment(department);
+    }
+    @GetMapping("/all")
+    public Map<Integer, List<Employee>> printAllEmployee() {
+        return departmentService.printAllEmployee();
+    }
 }
